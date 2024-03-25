7c2b1fbeddbde65c26ccafef624c055a9b41b0e2
MeerKar <meku.jithu@gmail.com>
2024-03-24 18:50:13 -0700

    new

--- server.js
+++ server.js
@@ -82,36 +82,36 @@ function Question() {
 // Viewing
 function viewDepartments() {
   const dbm = `SELECT department.id, department.name AS Department FROM department;`;
-  db.query(dbm, (err, res) => {
+  db.query(dbm, (err, results) => {
     if (err) {
       console.log(err);
       return;
     }
-    console.table(res);
+    console.table(results);
     Question();
   });
 }
 
 function viewRoles() {
   const dbm = `SELECT role.id, role.title AS role, role.salary, department.name AS department FROM role INNER JOIN department ON (department.id = role.department_id);`;
-  db.query(dbm, (err, res) => {
+  db.query(dbm, (err, results) => {
     if (err) {
       console.log(err);
       return;
     }
-    console.table(res);
+    console.table(results);
     Question();
   });
 }
 
 function viewEmployees() {
   const dbm = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id;`;
-  db.query(dbm, (err, res) => {
+  db.query(dbm, (err, results) => {
     if (err) {
       console.log(err);
       return;
     }
-    console.table(res);
+    console.table(results);
     Question();
   });
 }
@@ -128,12 +128,13 @@ function addDepartment() {
     ])
     .then((answer) => {
       const dbm = `INSERT INTO department(name) VALUES('${answer.department}');`;
-      db.query(dbm, (err, res) => {
+      db.query(dbm, (err, results) => {
         if (err) {
           console.log(err);
           return;
         }
         console.log("Added " + answer.department + " to the database");
+        console.table(results);
         Question();
       });
     });
@@ -167,12 +168,13 @@ function addRole() {
       ])
       .then((answers) => {
         const dbm = `INSERT INTO role SET title='${answers.title}', salary= ${answers.salary}, department_id= ${answers.department};`;
-        db.query(dbm, (err, res) => {
+        db.query(dbm, (err, results) => {
           if (err) {
             console.log(err);
             return;
           }
           console.log("Added " + answers.title + " to the database");
+          console.table(results);
           Question();
         });
       });
@@ -220,7 +222,7 @@ function addEmployee() {
         ])
         .then((answers) => {
           const dbm = `INSERT INTO employee SET first_name='${answers.first}', last_name= '${answers.last}', role_id= ${answers.role}, manager_id=${answers.manager};`;
-          db.query(dbm, (err, res) => {
+          db.query(dbm, (err, results) => {
             if (err) {
               console.log(err);
               return;
@@ -228,6 +230,7 @@ function addEmployee() {
             console.log(
               "Added " + answers.first + " " + answers.last + " to the database"
             );
+            console.table(results);
             Question();
           });
         });
@@ -299,12 +302,13 @@ function updateRole() {
         ])
         .then((answers) => {
           const dbm = `UPDATE employee SET role_id= ${answers.role}, manager_id=${answers.manager} WHERE id =${answers.employee};`;
-          db.query(dbm, (err, res) => {
+          db.query(dbm, (err, results) => {
             if (err) {
               console.log(err);
               return;
             }
             console.log("Employee role updated");
+            console.table(results);
             Question();
           });
         });
